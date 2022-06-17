5cf5903241a2d841c4c0a23725c805a2885e6127
DongHwan Kim <kdhpopoya@gmail.com>
2022-06-17 15:54:22 +0900

    chore(deps): Added dependencies
    - org.springframework:spring-web
    - com.amazonaws:aws-java-sdk-s3

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,11 +21,13 @@ dependencies {
     implementation("io.github.classgraph:classgraph:4.8.147")
     implementation("com.fasterxml.jackson.core:jackson-databind:2.13.3")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.3")
-    compileOnly("org.springframework.boot:spring-boot-autoconfigure:2.7.0")
+    implementation("org.springframework.boot:spring-boot-autoconfigure:2.7.0")
+    implementation("org.springframework:spring-web:5.3.20")
+    implementation("com.amazonaws:aws-java-sdk-s3:1.12.241")
 
     testImplementation("junit:junit:4.13.2")
     testImplementation("org.hamcrest:hamcrest:2.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-engine:5.8.2")
 }
 
 tasks {
