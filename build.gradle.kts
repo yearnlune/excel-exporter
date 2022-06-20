7b35d1c9bf06d33eb44e5681a4a8627d06a71bae
DongHwan Kim <kdhpopoya@gmail.com>
2022-06-20 17:13:58 +0900

    feat: Added publish task to gradle
    - added `publishing` task
    - renamed ci file name
    - added publish task to ci file

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,10 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     kotlin("jvm") version "1.6.20"
 
+    id("org.jetbrains.dokka") version "1.6.20"
     jacoco
+    `maven-publish`
+    signing
 }
 
 allprojects {
@@ -13,6 +16,10 @@ allprojects {
     repositories {
         mavenCentral()
         maven { url = uri("https://repo.spring.io/milestone") }
+    }
+
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "1.8"
     }
 }
 
@@ -32,14 +39,76 @@ dependencies {
     testImplementation("org.springframework.boot:spring-boot-starter-test:2.7.0")
 }
 
+val dokkaJavadocJar by tasks.register<Jar>("dokkaJavadocJar") {
+    dependsOn(tasks.dokkaJavadoc)
+    from(tasks.dokkaJavadoc.flatMap { it.outputDirectory })
+    archiveClassifier.set("javadoc")
+}
+
 tasks {
     test {
         useJUnitPlatform()
 
         finalizedBy(jacocoTestReport)
     }
+    java {
+        withSourcesJar()
+        dokkaJavadocJar
+    }
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.jvmTarget = "1.8"
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifact(tasks["sourcesJar"])
+            artifact(tasks["dokkaJavadocJar"])
+            pom {
+                name.set(rootProject.name)
+                description.set("A concise description of my library")
+                url.set("https://github.com/yearnlune/excel-exporter")
+                properties.set(mapOf(
+                    "myProp" to "value",
+                    "prop.with.dots" to "anotherValue"
+                ))
+                licenses {
+                    license {
+                        name.set("MIT License")
+                        url.set("https://opensource.org/licenses/MIT")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("yearnlune")
+                        name.set("DONGHWAN KIM")
+                        email.set("kdhpopyoa@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:https://github.com/yearnlune/excel-exporter.git")
+                    developerConnection.set("scm:git:ssh://git@github.com:yearnlune/excel-exporter.git")
+                    url.set("https://github.com/yearnlune/excel-exporter")
+                }
+            }
+        }
+
+    }
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+            configure<SigningExtension> {
+                val signingKey: String? by project
+                val signingPassword: String? by project
+                println("SECRET:\n $signingKey")
+                println("signingPassword:\n $signingPassword")
+                useInMemoryPgpKeys(signingKey, signingPassword)
+                sign(publications["mavenJava"])
+            }
+            credentials {
+                username = System.getenv("SONATYPE_USERNAME")
+                password = System.getenv("SONATYPE_PASSWORD")
+            }
+        }
+    }
 }
