990c4071549f1ee7134a190b2a51e609e6b92bab
DongHwan Kim <kdhpopoya@gmail.com>
2022-06-24 12:12:37 +0900

    fix: ExcelExporter 예외 처리
    - `NotFoundCreatorException` 처리

--- src/main/kotlin/io/github/yearnlune/excel/ExcelExporter.kt
+++ src/main/kotlin/io/github/yearnlune/excel/ExcelExporter.kt
@@ -17,20 +17,14 @@ class ExcelExporter(
 
     private val excelCreatorFactory: ExcelCreatorFactory = ExcelCreatorFactory()
 
-    fun exportAsWorkbook(data: ExcelMeta, creatorName: String = DEFAULT_CREATOR_NAME): Workbook? {
-        var workbook: Workbook? = null
 
-        try {
-            workbook = this.excelCreatorFactory.getCreator(creatorName).createExcel(data)
-        } catch (e: Exception) {
-            e.printStackTrace()
-        }
-
-        return workbook
+    @kotlin.jvm.Throws(NotFoundCreatorException::class)
+    fun exportAsWorkbook(data: ExcelMeta, creatorName: String = DEFAULT_CREATOR_NAME): Workbook {
+        return this.excelCreatorFactory.getCreator(creatorName).createExcel(data)
     }
-
-    fun exportAsByteString(data: ExcelMeta, creatorName: String = DEFAULT_CREATOR_NAME): String? {
-        return this.exportAsWorkbook(data, creatorName)?.let { toByteString(it) }
+    @kotlin.jvm.Throws(NotFoundCreatorException::class)
+    fun exportAsByteString(data: ExcelMeta, creatorName: String = DEFAULT_CREATOR_NAME): String {
+        return toByteString(this.exportAsWorkbook(data, creatorName))
     }
 
     fun exportAsResponseEntity(
@@ -38,19 +32,24 @@ class ExcelExporter(
         fileName: String? = "excel_exporter_data",
         creatorName: String = DEFAULT_CREATOR_NAME
     ): ResponseEntity<ByteArray> {
-        return this.exportAsWorkbook(data, creatorName)
-            ?.let {
-                ResponseEntity.ok()
-                    .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
-                    .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$fileName.xlsx")
-                    .body(toOutputStream(it).toByteArray())
-            } ?: ResponseEntity.badRequest().build()
+        return try {
+            this.exportAsWorkbook(data, creatorName)
+                .let {
+                    ResponseEntity.ok()
+                        .contentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"))
+                        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=$fileName.xlsx")
+                        .body(toOutputStream(it).toByteArray())
+                }
+        } catch (_: Exception) {
+            ResponseEntity.badRequest().build()
+        }
     }
 
+    @kotlin.jvm.Throws(NotFoundCreatorException::class)
     fun exportAsDownloadUrl(data: ExcelMeta, fileName: String = UUID.randomUUID().toString(), creatorName: String = DEFAULT_CREATOR_NAME): String? {
         return excelExporterConfigurer?.getS3Support()?.uploadAndGetDownloadUrl(
             fileName,
-            this.exportAsWorkbook(data, creatorName)!!
+            this.exportAsWorkbook(data, creatorName)
         )
     }
 
--- src/test/kotlin/io/github/yearnlune/excel/ExcelExporterTest.kt
+++ src/test/kotlin/io/github/yearnlune/excel/ExcelExporterTest.kt
@@ -3,9 +3,10 @@ package io.github.yearnlune.excel
 import org.apache.poi.ss.usermodel.WorkbookFactory
 import org.hamcrest.MatcherAssert.assertThat
 import org.hamcrest.Matchers.`is`
-import org.hamcrest.Matchers.nullValue
+import org.hamcrest.core.Is
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.io.ByteArrayInputStream
 import java.time.LocalDateTime
 import java.util.*
@@ -28,7 +29,7 @@ class ExcelExporterTest {
     fun exportAsWorkbook_withDefaultTemplate() {
         /* WHEN */
         val workBook = this.excelExporter.exportAsWorkbook(this.excelMeta)
-        val sheet = workBook?.getSheetAt(0)
+        val sheet = workBook.getSheetAt(0)
         val row = sheet?.getRow(0)
         val cell = row?.getCell(0)
 
@@ -41,7 +42,7 @@ class ExcelExporterTest {
     fun exportAsWorkbook_withSpecificTemplate() {
         /* WHEN */
         val workBook = this.excelExporter.exportAsWorkbook(this.excelMeta, "YEARNLUNE")
-        val sheet = workBook?.getSheetAt(0)
+        val sheet = workBook.getSheetAt(0)
         val row = sheet?.getRow(0)
         val cell = row?.getCell(0)
 
@@ -52,21 +53,23 @@ class ExcelExporterTest {
     @Test
     @DisplayName("정의되지 않은 템플릿 엑셀로 추출하기")
     fun exportAsWorkbook_withNotExistTemplate() {
-        /* WHEN */
-        val workBook = this.excelExporter.exportAsWorkbook(this.excelMeta, "NOT_EXIST")
-        val sheet = workBook?.getSheetAt(0)
-        val row = sheet?.getRow(0)
-        val cell = row?.getCell(0)
+        /* GIVEN */
+        val creatorName = "NOT_EXIST"
+
+        val exception = assertThrows<NotFoundCreatorException> {
+            /* WHEN */
+            this.excelExporter.exportAsWorkbook(this.excelMeta, creatorName)
+        }
 
         /* THEN */
-        assertThat(null, `is`(cell?.stringCellValue))
+        assertThat(exception.message, Is.`is`("COULD NOT FOUND [$creatorName] Excel Creator"))
     }
 
     @Test
     @DisplayName("String 타입으로 엑셀 추출하기")
     fun exportAsByteString_withDefaultTemplate_withBase64() {
         /* WHEN */
-        val byteString = this.excelExporter.exportAsByteString(data = this.excelMeta)!!
+        val byteString = this.excelExporter.exportAsByteString(data = this.excelMeta)
         val workBook = WorkbookFactory.create(ByteArrayInputStream(Base64.getDecoder().decode(byteString)))
         val sheet = workBook?.getSheetAt(0)
         val row = sheet?.getRow(0)
@@ -79,11 +82,16 @@ class ExcelExporterTest {
     @Test
     @DisplayName("String 타입으로 정의되지 않은 템플릿의 엑셀 추출하기")
     fun exportAsByteString_withNotExistTemplate() {
-        /* WHEN */
-        val byteString = this.excelExporter.exportAsByteString(this.excelMeta, "NOT_EXIST")
+        /* GIVEN */
+        val creatorName = "NOT_EXIST"
+
+        val exception = assertThrows<NotFoundCreatorException> {
+            /* WHEN */
+            this.excelExporter.exportAsByteString(this.excelMeta, creatorName)
+        }
 
         /* THEN */
-        assertThat(byteString, nullValue())
+        assertThat(exception.message, Is.`is`("COULD NOT FOUND [$creatorName] Excel Creator"))
     }
 
     @Test
@@ -99,8 +107,11 @@ class ExcelExporterTest {
     @Test
     @DisplayName("ResponseEntity 타입으로 정의되지 않은 템플릿의 엑셀 추출하기")
     fun exportAsResponseEntity_withNotExistTemplate() {
+        /* GIVEN */
+        val creatorName = "NOT_EXIST"
+
         /* WHEN */
-        val responseEntity = this.excelExporter.exportAsResponseEntity(this.excelMeta, "UNKNOWN_FILE", "UNKNOWN")
+        val responseEntity = this.excelExporter.exportAsResponseEntity(this.excelMeta, "UNKNOWN_FILE", creatorName)
 
         /* THEN */
         assertThat(400, `is`(responseEntity.statusCodeValue))
